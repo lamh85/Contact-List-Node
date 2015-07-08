f850a18701f547498be211e1e8d89694799b5e11
Hon Lam <lamh85@gmail.com>
2015-07-07 23:47:36 -0700

    Created ng Controller. Interfaced it with server.js

--- server.js
+++ server.js
@@ -7,5 +7,14 @@ var app = express();
 // Look for static file
 app.use(express.static(__dirname + "/public"));
 
+app.get('/contactlist', function(req, res) {
+  // This should appear in Terminal
+  console.log("I received a get request");
+
+  scores = [1,2,3];
+  // Generate a response by generating the "scores" variable in a JSON format
+  res.json(scores);
+});
+
 app.listen(3000);
 console.log("Server running on port 3000");
